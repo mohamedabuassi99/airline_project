7d70c07fda0be5005759530eff8870be59171863
mohamedabuassi99 <mohamed.abuassi99@gmail.com>
2020-04-23 23:21:49 +0300

    edit name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    AirLine
                 </div>
 
                 <div class="links">
